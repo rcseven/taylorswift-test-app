3db012364645527cb5ca104b24f56db8c41a59d9
Ryan Edquila <rcedquila@gmail.com>
2023-09-15 02:26:24 +0800

    Add Threshold Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Form from "./components/Form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
+import "./styles.css";
 
 function App() {
   const [decision, setDecision] = useState(null);
+  const [value, setValue] = useState(0);
   const handleFormSubmitTotal = (selectedOptions) => {
+
     const mappedData = {
       question1: selectedOptions.question1 * 0.6,
       question2: selectedOptions.question2 * 0.1,
@@ -19,10 +24,25 @@ function App() {
   return (
     <>
       <h1>Paladesisyon: Taylor Sheeeesh Concert</h1>
+      <p>Set threshold:</p>
+      <RangeSlider
+        className="single-thumb"
+        min={0}
+        max={2.1}
+        step={0.01}
+        defaultValue={[0]}
+        thumbsDisabled={[true, false]}
+        rangeSlideDisabled={true}
+        onInput={(values) => {
+          setValue(values[1])
+        }}
+      />
+
+      <p>θ = {value}</p>
+      {console.log(value)}
       <Form onSubmit={handleFormSubmitTotal} />
-      <p>θ = 1.8</p>
       {decision !== null ? (
-        decision >= 1.8 ? (
+        decision >= value ? (
           <p>θ &lt;= {decision}. Therefore, you can go to the concert!</p>
         ) : (
           <p>θ &gt; {decision}. Sorry, you can't go to the concert!</p>
